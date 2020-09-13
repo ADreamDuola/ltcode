c0eac4bc56d33171b55add1c0effd2d6afc1751d
唐欢 <1542927070@qq.com>
2020-09-13 11:03:11 +0800

    first test

--- src/com/tang/code/Hello.java
+++ src/com/tang/code/Hello.java
@@ -1,8 +1,11 @@
 package com.tang.code;
 
+import sun.lwawt.macosx.CSystemTray;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("tanghuanaichipi");
+        System.out.println("maogelovelaopo");
     }
 
 }
