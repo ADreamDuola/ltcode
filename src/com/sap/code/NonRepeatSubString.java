a1838d5178831ecde6319f0823188913f28d2d43
i321321 <yubo.mao@sap.com>
2020-12-12 11:28:38 +0800

    add ingore

--- src/com/sap/code/NonRepeatSubString.java
+++ src/com/sap/code/NonRepeatSubString.java
@@ -10,6 +10,8 @@ public class NonRepeatSubString {
     for(int start =0,end =0;end <str.length()&&start<str.length();end++){
       if(m.containsKey(str.charAt(end)))
       {
+
+
         start = Math.max(start,m.get(str.charAt(end)));
       }
       m.put(str.charAt(end),end+1);
