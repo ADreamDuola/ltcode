f055e7da5ef18f46c854bc039e956031b19d4ed4
i321321 <yubo.mao@sap.com>
2020-12-20 11:43:39 +0800

    00

--- src/com/sap/code/oddEvenList.java
+++ src/com/sap/code/oddEvenList.java
@@ -5,7 +5,10 @@ public class oddEvenList {
     System.out.println("pull test");
     System.out.println("merger test");
     System.out.println("hello case");
-//    System.out.println("fetch test2233556--");
+    System.out.println("fetch test2233556--");
+    System.out.println("testtest");
+    System.out.println("testtest");
+    System.out.println("testtest");
     System.out.println("fetch test88--1111");//tanghuan git fetch
     System.out.println("fetch test00--");//tanghuan git pull
     System.out.println("fetch test333--");//tanghuan git pull
